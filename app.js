36584de711798b5b1b3d0d85474d30f1d89c9863
shumphries11 <santitahumphries@gmail.com>
2022-03-08 15:10:55 -0600

    Added comments and styling

--- app.js
+++ app.js
@@ -11,13 +11,16 @@ let diceArr = [];
 
 class Die {
     constructor() {
-        this.roll();
+        // Create the die and append to the document
         this.div = document.createElement('div');
         this.div.className = "dieBox"
+        this.roll();
         this.div.innerHTML = this.value;
         container.appendChild(this.div);
+
         console.log(this);
 
+        //when the roll dice button is clicked changes all the values of each die in the document to another random number
         btn2.addEventListener('click', () => {
             console.log("clicked");
             diceArr.forEach(() => {
@@ -26,12 +29,13 @@ class Die {
             })
         })
 
+        // when a die is clicked changes the value to another random value
         this.div.addEventListener('click', () => {
             this.roll();
             this.div.innerHTML = this.value;
 
         })
-
+        // deletes the div that is double clicked from the html document and removes it from the array
         this.div.addEventListener('dblclick', () => {
             this.div.remove();
             let index = diceArr.indexOf(this);
@@ -43,7 +47,7 @@ class Die {
 
     }
 
-
+    // assigns a random value to each die
     roll() {
         this.value = Math.floor(Math.random() * 6) + 1;
     }
@@ -51,13 +55,14 @@ class Die {
 
 }
 
-
+// adds a new die to the html document and stores each class die created into the empty array named diceArr
 btn1.addEventListener('click', function () {
     diceArr.push(new Die);
     console.log(diceArr);
     console.log("clicked");
 })
 
+// add sums the values in the array
 function sum(arr) {
     let sum = 0;
     for (i = 0; i < arr.length; i++) {
@@ -66,6 +71,7 @@ function sum(arr) {
     return sum;
 }
 
+// passes diceArr into the sum function and alerts the reuslt
 sumDice.addEventListener('click', () => {
     let result = sum(diceArr);
     alert(`The sum of the dice is ${result}.`);
